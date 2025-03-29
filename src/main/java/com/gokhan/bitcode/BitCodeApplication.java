7ce49df1287ce6d6bc546be02a669635fdad7189
gokhangnccn <gokhangnccn@gmail.com>
2025-03-29 16:38:39 +0300

    init project

---
+++ src/main/java/com/gokhan/bitcode/BitCodeApplication.java
@@ -0,0 +1,13 @@
+package com.gokhan.bitcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokhan/bitcode/BitCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokhan.bitcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
